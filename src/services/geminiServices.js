3e7a15540b477e985cf511202e94b6560b39e9da
L4r1ss4Cr4v0 <larissacravoccamara@gmail.com>
2024-11-26 19:25:55 -0300

    fix: prompt syntax

--- src/services/geminiServices.js
+++ src/services/geminiServices.js
@@ -5,7 +5,7 @@ const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 export default async function gerarDescricaoComGemini(imageBuffer) {
   const prompt =
-    "Gere uma descrição em uma frase em português do brasil para a seguinte imagem";
+    "Gere uma descrição sem quebra de linha em uma frase em português do Brasil com 10 palavras no máximo para a seguinte imagem";
 
   try {
     const image = {
